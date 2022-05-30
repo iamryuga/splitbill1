b63004f0046316437fd31abbe16fda1a72e5ddd9
iamryuga <kumarshi605@gmail.com>
2022-05-30 09:43:00 +0530

    update registration

--- app/src/main/java/com/example/splitamount/Login.java
+++ app/src/main/java/com/example/splitamount/Login.java
@@ -2,13 +2,21 @@ package com.example.splitamount;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.LinearLayout;
+import android.widget.Toast;
 
 public class Login extends AppCompatActivity {
 
+    public void gotoRegistration(View view){
+        startActivity(new Intent(Login.this,Registration.class));
+    }
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.login);
+
     }
 }
