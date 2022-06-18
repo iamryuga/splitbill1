7fa011fdfaad4ae8ae5f919ab6ddac31f33846ac
iamryuga <kumarshi605@gmail.com>
2022-06-18 15:47:18 +0530

    pulled in mac

--- app/src/main/java/com/example/splitamount/Login.java
+++ app/src/main/java/com/example/splitamount/Login.java
@@ -2,21 +2,31 @@ package com.example.splitamount;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
+import android.widget.Button;
 import android.widget.LinearLayout;
+import android.widget.TextView;
 import android.widget.Toast;
 
 public class Login extends AppCompatActivity {
 
-    public void gotoRegistration(View view){
-        startActivity(new Intent(Login.this,Registration.class));
-    }
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.login);
-
+        TextView username = findViewById(R.id.username);
+        TextView me = findViewById(R.id.password);
+        Button but = findViewById(R.id.login);
+        but.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Log.i("fjd",username.getText().toString());
+            }
+        });
     }
 }
